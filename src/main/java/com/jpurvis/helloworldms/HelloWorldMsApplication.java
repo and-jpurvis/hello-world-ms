c827356880b191a99c4e85a578873af4955fb34d
Jim Purvis <james.purvis@and.digital>
2022-07-26 10:22:03 +0100

    first commit

---
+++ src/main/java/com/jpurvis/helloworldms/HelloWorldMsApplication.java
@@ -0,0 +1,13 @@
+package com.jpurvis.helloworldms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jpurvis/helloworldms/HelloWorldMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpurvis.helloworldms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
